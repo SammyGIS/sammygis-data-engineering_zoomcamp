a441447dcc3ff7c0458769949910e1c32425a07d
SammyGIS <adedoyinsamuel25@gmail.com>
2024-01-20 23:34:30 +0100

    add google bucket

--- week_1_basics_n_setup/1_terraform_gcp/main.tf
+++ week_1_basics_n_setup/1_terraform_gcp/main.tf
@@ -1,12 +1,38 @@
 terraform {
   required_providers {
     google = {
-      source  = "hashicorp/google"
+      source = "hashicorp/google"
       version = "5.12.0"
     }
   }
 }
 
 provider "google" {
-  # Configuration options
+  project     = "my-project-id"
+  region      = "us-central1"
+}
+
+
+resource "google_storage_bucket" "auto-expire" {
+  name          = "auto-expiring-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
 }
