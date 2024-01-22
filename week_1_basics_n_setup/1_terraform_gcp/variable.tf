b3d354814a5323863c13beeb6e13596c9a7f232c
SammyGIS <adedoyinsamuel25@gmail.com>
2024-01-22 21:26:26 +0100

    creating variable in terraform

---
+++ week_1_basics_n_setup/1_terraform_gcp/variable.tf
@@ -0,0 +1,22 @@
+variable "location"{
+    description ="Project location"
+    default = "US"
+}
+variable "bq_dataset_name"{
+    description ="My BigQuery Dataset Name"
+    default = "demo_dataset"
+}
+variable "gcs_storage_name"{
+    description ="My storage bucket name"
+    default = "demo_dataset"
+}
+
+variable "gcs_storage_class"{
+    description ="Bucket Storage Class"
+    default = "STANDARD"
+}
+
+variable "gcs_storage_class"{
+    description ="Bucket Storage Class"
+    default = "demo_dataset"
+}
