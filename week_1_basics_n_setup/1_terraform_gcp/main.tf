618ac68a07eddb73d233966a37033f1f108b95ff
SammyGIS <adedoyinsamuel25@gmail.com>
2024-02-01 22:28:53 +0100

    update

--- week_1_basics_n_setup/1_terraform_gcp/main.tf
+++ week_1_basics_n_setup/1_terraform_gcp/main.tf
@@ -1,15 +1,15 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "5.12.0"
     }
   }
 }
 
 provider "google" {
-  project     = "my-project-id"
-  region      = "us-central1"
+  project = "data-enginerring-zoomcamp"
+  region  = "us-central1"
 }
 
 
@@ -38,5 +38,5 @@ resource "google_storage_bucket" "demo-bucket" {
 }
 
 resource "google_bigquery_dataset" "demo_dataset" {
-  dataset_id                  = "demo_dataset"
+  dataset_id = "demo_dataset"
 }
