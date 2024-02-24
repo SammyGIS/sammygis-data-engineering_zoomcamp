d472c3128b0bafe294314477fa942901d5ad6f05
SammyGIS <adedoyinsamuel25@gmail.com>
2024-02-24 13:56:13 +0100

    update the script

--- week_2_workflow_orchestration/python_script/web_to_gcs.py
+++ week_2_workflow_orchestration/python_script/web_to_gcs.py
@@ -11,11 +11,11 @@ Pre-reqs:
 3. Set GCP_GCS_BUCKET as your bucket or change default value of BUCKET
 """
 
-services = ['fhv','green','yellow']
-year = [2019,2020]
+services = ['fhv', 'green', 'yellow']
+year = [2019, 2020]
 init_url = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/'
 # switch out the bucketname
-BUCKET = os.environ.get("GCP_GCS_BUCKET", "dtc-data-lake-bucketname")
+BUCKET = os.environ.get("GCP_GCS_BUCKET", "mage-zoomcamp-bucket-sammy")
 
 
 def upload_to_gcs(bucket, object_name, local_file):
@@ -34,33 +34,49 @@ def upload_to_gcs(bucket, object_name, local_file):
 
 
 def web_to_gcs(year, service):
-    for i in range(12):
-        
-        # sets the month part of the file_name string
-        month = '0'+str(i+1)
-        month = month[-2:]
+    # for i in range(12):
 
-        # csv file_name
-        file_name = f"{service}_tripdata_{year}-{month}.csv.gz"
+    #     # sets the month part of the file_name string
+    #     month = '0' + str(i + 1)
+    #     month = month[-2:]
 
-        # download it using requests via a pandas df
-        request_url = f"{init_url}{service}/{file_name}"
-        r = requests.get(request_url)
-        open(file_name, 'wb').write(r.content)
-        print(f"Local: {file_name}")
+    #     # csv file_name
+    #     file_name = f"{service}_tripdata_{year}-{month}.csv.gz"
 
-        # read it back into a parquet file
-        df = pd.read_csv(file_name, compression='gzip')
-        file_name = file_name.replace('.csv.gz', '.parquet')
-        df.to_parquet(file_name, engine='pyarrow')
-        print(f"Parquet: {file_name}")
+    #     # if os.path.exists(file_name):
+    #     #     print (f"File {file_name} already exists. Skipping...")
+    #     #     continue # Skip the current iteration and move to the next one
 
-        # upload it to gcs 
-        upload_to_gcs(BUCKET, f"{service}/{file_name}", file_name)
-        print(f"GCS: {service}/{file_name}")
+    # download it using requests via a pandas df
+    # request_url = f"{init_url}{service}/{file_name}"
+    # print(request_url)
+    # r = requests.get(request_url)
+    # print(r)
+    # print(r.status_code)
+    # open(file_name, 'wb').write(r.content)
+    # print(f"Local: {file_name}")
+
+ # read it back into a parquet file
+ # df = pd.read_csv(file_name, compression='gzip', encoding='latin1')
+
+    df = pd.read_csv(r'C:\Users\HP\Desktop\data-engineering-zoomcamp\week_2_workflow_orchestration\python_script\fhv_tripdata_2020-02.csv.gz',
+                compression='gzip', encoding='latin1')
+    file_name = 'fhv_tripdata_2020-02.parquet'
+    #file_name = file_name.replace('.csv.gz', '.parquet')
+    df.to_parquet(file_name, engine='pyarrow')
+    print(f"Parquet: {file_name}")
+
+    # upload it to gcs
+    upload_to_gcs(BUCKET, f"{service}/{file_name}", file_name)
+    print(f"GCS: {service}/{file_name}")
 
 
-web_to_gcs('2019', 'green')
-web_to_gcs('2020', 'green')
-web_to_gcs('2019', 'yellow')
-web_to_gcs('2020', 'yellow')
+# web_to_gcs('2019', 'yellow')
+# web_to_gcs('2020', 'yellow')
+
+
+# web_to_gcs('2019', 'green')
+# web_to_gcs('2020', 'green')
+
+web_to_gcs('2019', 'fhv')
+web_to_gcs('2020', 'fhv')
